b39ff083889298f216b66d9fe82cd0c058c3b3f9
unknown <Administrator@HOPC560.v2rd.com>
2026-03-26 08:19:18 +0530

    add host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': { target: 'http://localhost:8000', changeOrigin: true }
-    }
+    },
+    allowedHosts: ['ars.v2retail.net']
   }
 })
